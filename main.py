61a4a3e073b8c6d95dad01a9a87e59d0756bf6cd
Tyujhty <fannybaquey@gmail.com>
2023-12-20 10:42:18 +0000

    feat: creating conditions depending of the choices of user

--- main.py
+++ main.py
@@ -26,5 +26,20 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-
-
+path = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n").lower()
+if path == 'right':
+  print("You fall into a hole. Game Over.")
+else:
+  lake = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim accross.\n").lower()
+  if lake == 'swim':
+    print("You are attacked by trout. Game Over")
+  else:
+    door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one bleu. Wich color do you choose?\n").lower()
+    if door == 'red':
+      print("You are burned by a fire. Game Over")
+    elif door == 'yellow':
+      print("You found the treasure! You WIN")
+    elif door == 'bleu':
+      print("You are eaten by beasts. Game Over")
+    else:
+      print("Game Over.")
