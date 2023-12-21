70aa62e696da385f5724a895deba4b06f45a587f
Tyujhty <fannybaquey@gmail.com>
2023-12-21 23:04:28 +0000

    feat: refactoring

--- main.py
+++ main.py
@@ -27,13 +27,9 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 path = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n").lower()
-if path == 'right':
-  print("You fall into a hole. Game Over.")
-else:
+if path == 'left':
   lake = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim accross.\n").lower()
-  if lake == 'swim':
-    print("You are attacked by trout. Game Over")
-  else:
+  if lake == 'wait':
     door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one bleu. Wich color do you choose?\n").lower()
     if door == 'red':
       print("You are burned by a fire. Game Over")
@@ -43,3 +39,8 @@ else:
       print("You are eaten by beasts. Game Over")
     else:
       print("Game Over.")
+  else:
+    print("You are attacked by trout. Game Over")
+else:
+  print("You fall into a hole. Game Over.")
+  
